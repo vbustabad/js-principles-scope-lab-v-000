4df51612f84c5397abccd81d2d1d431ac0d4a764
Veronica Bustabad <vbustabad@gmail.com>
2018-09-03 02:43:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase();
+
+  return upperCaseCustomerName
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'sheryl'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'julia';
+}
